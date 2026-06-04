0bad043b2bc19ca8084c831d40b32786d443952b
Malzeir <hikmalpaijo2017@gmail.com>
2026-06-05 00:06:34 +0700

    final fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("docs"));
 
 app.listen(3000, () => {
   console.log("Server running di http://localhost:3000");
